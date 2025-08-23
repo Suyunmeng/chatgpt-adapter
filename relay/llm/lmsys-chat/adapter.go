95f2ec5d8c660d91fcb57bcc8c80ef2745baf08c
Suyunmeng <Susus0175@proton.me>
2025-08-23 17:13:57 +0800

    update models

--- relay/llm/lmsys-chat/adapter.go
+++ relay/llm/lmsys-chat/adapter.go
@@ -19,6 +19,7 @@ var (
 		"gpt-5-chat":                              "4b11c78c-08c8-461c-938e-5fc97d56a40d",
 		"gpt-5-mini-high":                         "5fd3caa8-fe4c-41a5-a22c-0025b58f4b42",
 		"gpt-4.1-2025-04-14":                      "14e9311c-94d2-40c2-8c54-273947e208b0",
+		"o3-2025-04-16":                           "cb0f1e24-e8e9-4745-aabc-b926ffde7475",
 		"o4-mini-2025-04-16":                      "f1102bbf-34ca-468f-a9fc-14bcf63f315b",
 		"gemini-2.5-pro":                          "e2d9d353-6dbe-4414-bf87-bd289d523726",
 		"claude-opus-4-20250514":                  "ee116d12-64d6-48a8-88e5-b2d06325cdd2",
